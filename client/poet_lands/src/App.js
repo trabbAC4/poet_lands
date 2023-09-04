1de0eb3a62ac87343eed434fd80ff26ed1ee8a3e
Tahmidur_Rabb <tr1476@nyu.edu>
2023-09-04 08:41:17 -0400

    Router

--- client/poet_lands/src/App.js
+++ client/poet_lands/src/App.js
@@ -1,44 +1,29 @@
-
 import './App.css';
+import Poem from "./Poem";
+import Header from "./Header";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <main>
-      <header> 
-        <a href= "" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className = "poem">
-        <img src=""></img>
-        <h2> Poet title </h2>
-        <p className="info">
-          <a className= "author"> Tahmidur Rabb</a>
-          <time> 2023-09-05 08:50</time>
-        </p>
-        <p className= "summary"> This is the poem</p>
-      </div>
-      <div className = "poem">
-        <img src=""></img>
-        <h2> Poet title </h2>
-        <p className="info">
-          <a className= "author"> Tahmidur Rabb</a>
-          <time> 2023-09-05 08:50</time>
-        </p>
-        <p className= "summary"> This is the poem</p>
-      </div>
-      <div className = "poem">
-        <img src=""></img>
-        <h2> Poet title </h2>
-        <p className="info">
-          <a className= "author"> Tahmidur Rabb</a>
-          <time> 2023-09-05 08:50</time>
-        </p>
-        <p className= "summary"> This is the poem</p>
-      </div>
-    </main>
+    <Routes>
+      <Route index element= {
+        <main>
+          <Header />
+          <Poem />
+          <Poem />
+          <Poem />
+        </main>
+
+      } />
+      <Route path= {'/login'} element= {
+        <div> Login Page </div>
+      } />
+      <Route path = {'/register'} element = {
+        <div> Register </div> 
+      }
+      />
+    </Routes>
+
   );
 }
 
---
+++ client/poet_lands/src/Poem.js
@@ -0,0 +1,14 @@
+export default function Poem() {
+    return(
+        <div className = "poem">
+        <img src=""></img>
+        <h2> Poet title </h2>
+        <p className="info">
+          <a className= "author"> Tahmidur Rabb</a>
+          <time> 2023-09-05 08:50</time>
+        </p>
+        <p className= "summary"> This is the poem</p>
+      </div>
+
+    )
+}
