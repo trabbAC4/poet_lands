596fb5f8b24115b1685d798ef377c1454f9c300d
Tahmidur_Rabb <tr1476@nyu.edu>
2023-09-05 13:34:42 -0400

    encryption dependancy

--- api/index.js
+++ api/index.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-//mongoose.connect('mongodb+srv://user1:y2CI8pkYB3ziK4cb@cluster0.2rkjome.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://user1:y2CI8pkYB3ziK4cb@cluster0.2rkjome.mongodb.net/?retryWrites=true&w=majority');
 
 
-app.post('/register', (req,res) =>  {
-    res.json('test okay');
-    console.log('test');
-    //const {username, password} = req.body;
+app.post('/register', async (req,res) =>  {
+    const {username, password} = req.body;
+    try{
+        const userDoc = await User.create({username,  password});
+        res.json(userDoc); 
+    } catch(e) {
+        res.status(400).json(e);
+    }
 });
 
 app.listen(4000);
--- api/models/User.js
+++ api/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const {Schema, model} = mongoose;
 
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, min: 4, unique: true},
@@ -7,4 +7,5 @@ const UserSchema = new mongoose.Schema({
 });
 
 const UserModel = model('User', UserSchema);
+
 module.exports = UserModel;
