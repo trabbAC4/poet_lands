67458d44130c829bcf18b66500837887277f6672
Tahmidur_Rabb <tr1476@nyu.edu>
2023-09-05 14:32:18 -0400

    logout functionality: encountering bug

--- api/index.js
+++ api/index.js
@@ -5,11 +5,13 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt= require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const jwt_secret= 'asdiibasdb12312'
 const salt= bcrypt.genSaltSync(10);
 app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://user1:y2CI8pkYB3ziK4cb@cluster0.2rkjome.mongodb.net/?retryWrites=true&w=majority');
 
@@ -41,6 +43,17 @@ app.post('/login', async (req,res)=> {
         res.status(400).json('wrong credentials');
     }
 
+});
+app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, jwt_secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req,res)=> {
+    res.cookie('token', '').json('ok');
 })
 app.listen(4000);
 
--- client/poet_lands/src/Header.js
+++ client/poet_lands/src/Header.js
@@ -1,9 +1,46 @@
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+
 export default function Header() {
+  const [username, setUsername] = useState(null);
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUsername(userInfo.username);
+      })
+
+    });
+  }, []);
+
+  function logout() {
+    fetch('http:localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST', 
+    })
+    setUsername(null);
+    
+  }
+
+
     return(
         <header> 
         <Link to= "/" className="logo">MyBlog</Link>
         <nav>
+          {username && (
+            <>
+              <Link to ="/create"> Create new poem </Link>
+              <a onClick = {logout}> Logout </a>
+            </>
+          )}
+          {!username && (
+            <>
+            <Link to = "/login"> Login </Link>
+            <Link to = "/register"> Register </Link>
+            </> 
+          )}
           <Link to="/login">Login</Link>
           <Link to="/register">Register</Link>
         </nav>
