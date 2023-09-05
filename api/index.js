b81a014e80693b8e1053c80063a0d9177a5d27b3
Tahmidur_Rabb <tr1476@nyu.edu>
2023-09-05 13:45:13 -0400

    Login functionality

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
 
+const salt= bcrypt.genSaltSync(10);
 app.use(cors());
 app.use(express.json());
 
@@ -14,7 +15,9 @@ mongoose.connect('mongodb+srv://user1:y2CI8pkYB3ziK4cb@cluster0.2rkjome.mongodb.
 app.post('/register', async (req,res) =>  {
     const {username, password} = req.body;
     try{
-        const userDoc = await User.create({username,  password});
+        const userDoc = await User.create({
+            username,  
+            password: bcrypt.hashSync(password, salt),});
         res.json(userDoc); 
     } catch(e) {
         res.status(400).json(e);
