4843c16f42381a20492d7f83eb0c46c1815ac4f0
Tahmidur_Rabb <tr1476@nyu.edu>
2023-09-05 14:06:13 -0400

    Login successful

--- api/index.js
+++ api/index.js
@@ -4,9 +4,11 @@ const { default: mongoose } = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
+const jwt= require('jsonwebtoken');
 
+const jwt_secret= 'asdiibasdb12312'
 const salt= bcrypt.genSaltSync(10);
-app.use(cors());
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://user1:y2CI8pkYB3ziK4cb@cluster0.2rkjome.mongodb.net/?retryWrites=true&w=majority');
@@ -24,6 +26,22 @@ app.post('/register', async (req,res) =>  {
     }
 });
 
+
+app.post('/login', async (req,res)=> {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        jwt.sign({username, id:userDoc._id}, jwt_secret, {}, (err,token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+    }
+    else{
+        res.status(400).json('wrong credentials');
+    }
+
+})
 app.listen(4000);
 
 //y2CI8pkYB3ziK4cb
--- client/poet_lands/src/pages/LoginPage.js
+++ client/poet_lands/src/pages/LoginPage.js
@@ -1,19 +1,32 @@
 import {useState} from "react";
+import {Navigate} from "react-router-dom";
 
 export default function LoginPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
     async function login(ev) {
         ev.preventDefault();
-        await fetch('http://localhost:4000/login', {
+        const response = await fetch('http://localhost:4000/login', {
             method: 'POST',
             body: JSON.stringify({username, password}),
             headers: {'Content-Type': 'application/json'},
-        })
+            credentials: 'include',
+        });
+        if (response.ok) {
+            setRedirect(true);
+
+        }
+        else {
+            alert('wrong credentials');
+        }
+    }
+    if (redirect) {
+        return <Navigate to ={'/'} />
     }
     return (
         <div>
-            <form className= "Login">
+            <form className= "Login" onSubmit={login}>
                 <h1> Login </h1> 
                 <input type= "text"
                      placeholder= "username"
