4ffab634b8676c32c27eb3495f3fd1943f7f535f
Tahmidur_Rabb <tr1476@nyu.edu>
2023-09-28 19:10:49 -0400

    Fixed this ridiculous bug :)

--- client/src/Poem.js
+++ client/src/Poem.js
@@ -1,15 +1,10 @@
-import {format} from "date-fns";
+import {formatISO9075} from "date-fns";
 
-export default function Poem(title, content, createdAt, author) {
+export default function Poem({title, content, author}) {
     return(
-        <div className = "poem">
-        <h2> {title} </h2>
-        <p className="info">
-          {author && author.username && <a className= "author"> {author.username} </a>}
-          <time> {format(new Date(createdAt), 'MM')}</time>
-        </p>
-        <p className= "summary"> {content} </p>
-
+      <div className="poem">
+        <h2> {title} </h2> 
+        <h2> {content} </h2>
       </div>
 
     )
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,21 +4,21 @@ import {useEffect, useState} from "react";
 
  export default function HomePage() {
      const [poems, setPoems] = useState([]);
+
      useEffect(() => {
          fetch('http://localhost:4000/poem').then(response => {
-             response.json().then(poems => {
-                 console.log(poems);
+             response.json().then((poems) => {
                  setPoems(poems);
-             });
-         });
-     }, []);
+                 });
+            });
+        
+     },[]);  
 
     return (
         <>
-        {poems.length > 0 && poems.map(poem => (
-            <Poem key=  {poem.id} {...poem} />
+           {poems.map(poem => (
+            <Poem  {...poem} />
         ))}
-
         </>
-    )
+    );
 }
